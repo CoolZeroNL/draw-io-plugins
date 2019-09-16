1deaed6ac4f27422e590d2394cfd50fa3a1519e3
CoolZeroNL <info@local-events.nl>
2019-09-16 10:33:13 +0200

    add

--- draw-io-plugin-resize.js
+++ draw-io-plugin-resize.js
@@ -41,68 +41,70 @@ Draw.loadPlugin(function(ui)
             console.log(defaultheight);
             console.log(defaultwidth);
             console.log(defaultstyle);
+
+            var cells = graph.getModel().cells;
+            //console.log(cells);
+            //console.log(cells.length);
+    
+            Object.keys(cells).forEach(function(key) {
+    
+                //console.log(key, cells[key]);
+                //console.log(key);
+                console.log(cells[key]);
+               // console.log(cells[key].style);
+               var cellstyle = cells[key].style;
+               
+               var geocurrentcell = graph.getCellGeometry(cells[key]);   
+    
+    
+                console.log('cellstyle');
+                console.log(cellstyle);
+                console.log(typeof cellstyle=="string");
+    
+                console.log('defaultstyle');
+                console.log(defaultstyle);
+                console.log(typeof defaultstyle=="string");
+    
+               if (typeof defaultstyle=="string" && typeof cellstyle=="string" && defaultstyle === cellstyle)
+                {
+    
+    
+                    console.log('chaning size !');
+                    // NO...
+                     // shape=process;
+                     // shape=mxgraph.flowchart.decision;
+                     // ellipse;
+                     // swimlane;
+                     // edgeStyle=orthogonalEdgeStyle;
+                     // shape=mxgraph.bpmn.shape;
+    
+                    // YES.... ( get style of selected ???) == var defaultstyle !
+                     // rounded=1;whiteSpace=wrap;html=1;shadow=0;labelBackgroundColor=none;strokeColor=#000000;strokeWidth=1;fillColor=#ffffff;fontFamily=Verdana;fontSize=8;fontColor=#000000;align=center;
+    
+    
+                    graph.getModel().beginUpdate();
+    
+                    // updating cell.
+                      // move cell
+                      geocurrentcell.height = defaultheight;
+                      geocurrentcell.width = defaultwidth;
+                        model.setGeometry(cells[key], geocurrentcell);
+            
+                     graph.getModel().endUpdate();
+                     graph.refresh(); // update the graph
+                }
+              
+            });
         }
 
-        var cells = graph.getModel().cells;
-        //console.log(cells);
-        //console.log(cells.length);
 
-        Object.keys(cells).forEach(function(key) {
-
-            //console.log(key, cells[key]);
-            //console.log(key);
-            console.log(cells[key]);
-           // console.log(cells[key].style);
-           var cellstyle = cells[key].style;
-           
-           var geocurrentcell = graph.getCellGeometry(cells[key]);   
-
-
-            console.log('cellstyle');
-            console.log(cellstyle);
-            console.log(typeof cellstyle=="string");
-
-            console.log('defaultstyle');
-            console.log(defaultstyle);
-            console.log(typeof defaultstyle=="string");
-
-           if (typeof defaultstyle=="string" && typeof cellstyle=="string" && defaultstyle === cellstyle)
-            {
-
-
-                console.log('chaning size !');
-                // NO...
-                 // shape=process;
-                 // shape=mxgraph.flowchart.decision;
-                 // ellipse;
-                 // swimlane;
-                 // edgeStyle=orthogonalEdgeStyle;
-                 // shape=mxgraph.bpmn.shape;
-
-                // YES.... ( get style of selected ???) == var defaultstyle !
-                 // rounded=1;whiteSpace=wrap;html=1;shadow=0;labelBackgroundColor=none;strokeColor=#000000;strokeWidth=1;fillColor=#ffffff;fontFamily=Verdana;fontSize=8;fontColor=#000000;align=center;
-
-
-                graph.getModel().beginUpdate();
-
-                // updating cell.
-                  // move cell
-                  geocurrentcell.height = 100;
-                  geocurrentcell.width = 50;
-                    model.setGeometry(cells[key], geocurrentcell);
-        
-                 graph.getModel().endUpdate();
-                 graph.refresh(); // update the graph
-            }
-          
-        });
 
         // for (var i = 0; i < cells.length; i++)
         // {
         //     console.log(cells[i]);
         // }
 
-    }, null, null, 'Alt+Shift+X - v0.993');
+    }, null, null, 'Alt+Shift+X - v0.994');
 
     
 }); // end of loadplugin
