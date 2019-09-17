d3dbe52bae1d56d769ced7856204671a29b9d767
CoolZeroNL <info@local-events.nl>
2019-09-17 12:26:20 +0200

    Auto Add: 17-09-2019 12:26:20 [skip ci]

--- draw-io-plugin-securit-huisstyle.js
+++ draw-io-plugin-securit-huisstyle.js
@@ -21,12 +21,14 @@ Draw.loadPlugin(function(ui)
 	// Adds custom sidebar entry
     ui.sidebar.addPalette('SecurITLibrary', 'SecurIT Customized', true, function(content)
  	{	
+		 // Shapes (existing)
 		content.appendChild(ui.sidebar.createVertexTemplate('shape=ext;rounded=1;strokeColor=#007FFF', 120, 80, 'Task', 'Task', true));		
 		content.appendChild(ui.sidebar.createVertexTemplate('shape=ext;rounded=1;strokeWidth=3;strokeColor=#007FFF', 120, 80, 'Call Activity', 'Call Activity', true));
 		content.appendChild(ui.sidebar.createVertexTemplate('shape=ext;rounded=1;symbol0=plus;symbol0Width=14;symbol0Height=14;symbol0Align=center;symbol0VerticalAlign=bottom;strokeColor=#007FFF', 120, 80, 'Subprocess', 'Subprocess', true));
 		
-		content.appendChild(ui.sidebar.createVertexTemplate('image;image=https://upload.wikimedia.org/wikipedia/commons/1/17/Yin_yang.svg', 100, 100));
-		content.appendChild(ui.sidebar.createVertexTemplate('image;image=https://upload.wikimedia.org/wikipedia/commons/1/17/Yin_yang.svg', 100, 100));
+		// load images from URL
+		content.appendChild(ui.sidebar.createVertexTemplate('image;image=https://raw.githubusercontent.com/CoolZeroNL/draw-io-plugins/master/securit.huisstyle.images/unicorn.svg', 100, 100));
+		content.appendChild(ui.sidebar.createVertexTemplate('image;image=https://raw.githubusercontent.com/CoolZeroNL/draw-io-plugins/master/securit.huisstyle.images/sit-header.svg', 100, 100));
 		
 		// content.appendChild(ui.sidebar.createVertexTemplate('shape=mxgraph.bpmn.shape;verticalLabelPosition=bottom;verticalAlign=top;perimeter=ellipsePerimeter;outline=standard;symbol=general;strokeColor=#009900', 60, 60, 'Start Event', 'Start Event', true));
 		// content.appendChild(ui.sidebar.createVertexTemplate('shape=mxgraph.bpmn.shape;verticalLabelPosition=bottom;verticalAlign=top;perimeter=ellipsePerimeter;outline=end;symbol=general;strokeColor=#FF0000', 60, 60, 'End Event', 'End Event', true));	
@@ -34,10 +36,12 @@ Draw.loadPlugin(function(ui)
 		// content.appendChild(ui.sidebar.createVertexTemplate('shape=mxgraph.bpmn.shape;verticalLabelPosition=bottom;verticalAlign=top;perimeter=rhombusPerimeter;background=gateway;outline=none;symbol=parallelGw;strokeColor=#CCCC00', 100, 100, 'Parallel Gateway', 'Parallel Gateway', true));
 		// content.appendChild(ui.sidebar.createVertexTemplate('shape=mxgraph.bpmn.shape;verticalLabelPosition=bottom;verticalAlign=top;perimeter=rhombusPerimeter;background=gateway;outline=end;symbol=general;strokeColor=#CCCC00', 100, 100, 'Inclusive Gateway', 'Inclusive Gateway', true));
 		// content.appendChild(ui.sidebar.createVertexTemplate('shape=mxgraph.bpmn.shape;verticalLabelPosition=bottom;verticalAlign=top;perimeter=rhombusPerimeter;background=gateway;outline=none;symbol=complexGw;strokeColor=#CCCC00', 100, 100, 'Complex Gateway', 'Complex Gateway', true));
-		
+
+		// Swimlanes (existing)
 		content.appendChild(ui.sidebar.createVertexTemplate('swimlane;horizontal=0;startSize=20', 180, 100, 'Pool', 'Pool', true));
 		content.appendChild(ui.sidebar.createVertexTemplate('swimlane;horizontal=0;swimlaneFillColor=white;swimlaneLine=0', 120, 80, 'Lane', 'Lane', true));
 		
+		// Arrows (existing)
 		content.appendChild(ui.sidebar.createEdgeTemplate('endArrow=block;endFill=1;endSize=6', 200, 100, 'Sequence Flow', 'Sequence Flow', true))
   	});
 	
