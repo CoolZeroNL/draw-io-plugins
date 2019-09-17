725c255c382bd2b1e43a25b375f8a33b2595553f
CoolZeroNL <info@local-events.nl>
2019-09-17 13:13:38 +0200

    Auto Add: 17-09-2019 13:13:38 [skip ci]

--- draw-io-plugin-tmp.js
+++ draw-io-plugin-tmp.js
@@ -15,32 +15,4 @@ Draw.loadPlugin(function(ui) {
     // Adds logo to footer
     ui.footerContainer.innerHTML = '<img align="right" style="margin-top:14px;margin-right:6px;" ' + 'src="https://www.google.com/favicon.ico"/>';
 
-    // Adds resource for action
-    mxResources.parse('helloWorldAction=Hello, World!');
-
-    // Adds action
-    ui.actions.addAction('helloWorldAction', function() {
-        var ran = Math.floor((Math.random()*100)+1);
-        mxUtils.alert('A random number is ' + ran);
-    });
-
-    // Adds menu
-    ui.menubar.addMenu('Hello, World Menu', function(menu, parent) {
-        ui.menus.addMenuItem(menu, 'helloWorldAction');
-    });
-
-    // Reorders menubar
-    ui.menubar.container.insertBefore(ui.menubar.container.lastChild,
-    ui.menubar.container.lastChild.previousSibling.previousSibling);
-
-    // Adds toolbar button
-    ui.toolbar.addSeparator();
-    var elt = ui.toolbar.addItem('', 'helloWorldAction');
-
-    // Cannot use built-in sprites
-    elt.firstChild.style.backgroundImage = 'url(https://www.draw.io/images/logo-small.gif)';
-    elt.firstChild.style.backgroundPosition = '2px 3px';
-
-    // Displays status message
-    ui.editor.setStatus('Hello, World!');
 });
