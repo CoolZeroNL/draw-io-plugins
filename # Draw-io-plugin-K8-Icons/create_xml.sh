a834a5bb54f1ecb53730efca379098e2b1431933
CoolZeroNL <info@local-events.nl>
2019-09-25 08:53:02 +0200

    Auto Add: 25-09-2019 08:53:01 [skip ci]

---
+++ # Draw-io-plugin-K8-Icons/create_xml.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+## need: sudo apt install gridsite-clients
+
+name='k8'
+filename=$name'.xml'
+
+## start:
+echo '<shapes name="stencils.'"$name"'">' > $filename
+
+
+cat readme.md | grep .svg | while read line
+do
+   # do something with $line here
+   item=`basename $line .svg`
+   url=`urlencode $line`
+   
+cat >>$filename <<EOL
+	<shape name="k8_icons_${item}" w="64" h="64" aspect="variable">
+		<connections>
+			<constraint x="0.500" y="0.000" perimeter="0" name="top-center"/>
+			<constraint x="0.000" y="0.000" perimeter="0" name="top-left"/>
+			<constraint x="1.000" y="0.000" perimeter="0" name="top-right"/>
+			<constraint x="1.000" y="0.500" perimeter="0" name="right-center"/>
+			<constraint x="0.000" y="0.500" perimeter="0" name="left-center"/>
+			<constraint x="0.000" y="1.000" perimeter="0" name="bottom-left"/>
+			<constraint x="1.000" y="1.000" perimeter="0" name="bottom-right"/>
+			<constraint x="0.500" y="1.000" perimeter="0" name="bottom-center"/>
+		</connections>
+		<foreground>
+			<image src="https://jsonp.afeld.me/?url=${url}%3Fsanitize%3Dtrue" x="0.00" y="0.00" w="64" h="64"/>
+		</foreground>
+	</shape>
+EOL
+
+done
+
+## End
+echo '</shapes>' >> $filename
+
+
