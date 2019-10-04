a5add52b9af9048df38167225e1563d9dacbd151
CoolZeroNL <info@local-events.nl>
2019-10-04 11:55:32 +0200

    Auto Add: 04-10-2019 11:55:32 [skip ci]

--- # Draw-io-plugin-SecurIT/draw-io-plugin-securit-huisstyle-v1.js
+++ # Draw-io-plugin-SecurIT/draw-io-plugin-securit-huisstyle-v1.js
@@ -2,17 +2,17 @@
 function appendStyle(styles) {
     var css = document.createElement('style');
     css.type = 'text/css';
-  
+
     if (css.styleSheet) css.styleSheet.cssText = styles;
     else css.appendChild(document.createTextNode(styles));
-  
+
     document.getElementsByTagName("head")[0].appendChild(css);
-  }
-  
+}
+
 //   var styles = "@font-face { font-family: 'Marvel'; src: url(/confluence/download/attachments/851969/Marvel-Regular.ttf?api=v2) format('truetype')}";
 //   styles += ' #content { color: blue; text-align: left; }';
 
-  var styles = `
+var styles = `
     @font-face {
         font-family: 'Libre Franklin';
         font-style: normal;
@@ -27,41 +27,47 @@ function appendStyle(styles) {
     }
   `;
 
-  
-  window.onload = function() { appendStyle(styles) };
+
+window.onload = function() { appendStyle(styles) };
 
 //############################################################################################################################
 
 /**
-* External plugin to be added to draw.io
-*/
+ * External plugin to be added to draw.io
+ */
 
 Draw.loadPlugin(function(ui) {
     // Adds custom sidebar entry
     //ui.sidebar.addStencilPalette('flowchart', 'Odoo flow', 'https://draw.io/templates/flowcharts/flowchart_1.xml',';fillColor=#ffffff;strokeColor=#000000;strokeWidth=2');
 
+    // Adds resource for action
+    mxResources.parse('sitrefont=Apply SecurIT Font to All Cells');
 
-    ui.sidebar.addStencilPalette('flowchart', 'SecurIT', 'https://yacdn.org/serve/https://raw.githubusercontent.com/CoolZeroNL/draw-io-plugins/master/%23%20Draw-io-plugin-SecurIT/stencil-securit.xml?maxAge=10',';fillColor=#ffffff;strokeColor=#000000;strokeWidth=2');
+    if (ui.editor.isChromelessView()) {
+        return;
+    }
+
+    // Adds menu
+    ui.menubar.addMenu('SecurIT', function(menu, parent) {
+        ui.menus.addMenuItem(menu, 'sitrefont');
+    });
+
+
+
+    // Add sidebar Stencil
+    ui.sidebar.addStencilPalette('flowchart', 'SecurIT', 'https://yacdn.org/serve/https://raw.githubusercontent.com/CoolZeroNL/draw-io-plugins/master/%23%20Draw-io-plugin-SecurIT/stencil-securit.xml?maxAge=10', ';fillColor=#ffffff;strokeColor=#000000;strokeWidth=2');
 
     // Collapses default sidebar entry and inserts this before
-
     var c = ui.sidebar.container;
     c.firstChild.click();
     c.insertBefore(c.lastChild, c.firstChild);
     c.insertBefore(c.lastChild, c.firstChild);
 
-    // $("img[title='draw.io']").src="http://poiesisconsulting.com/favicon.ico";
-    // Adds logo to footer
-    // doest work
-    //ui.footerContainer.innerHTML = '<img align="right" style="margin-top:14px;margin-right:6px;" ' + 'src="https://www.google.com/favicon.ico"/>';
-
-
-
-// LOAD FONTS:
+    // LOAD FONTS:
 
     // get current custom fonts
     const curfonts = ui.menus.customFonts;
-    
+
     // add Libre Franklin
     curfonts.push("Libre Franklin");
 
@@ -70,5 +76,58 @@ Draw.loadPlugin(function(ui) {
 
     // add manuel
     //ui.menus.customFonts = ["Libre Franklin"];
+
+
+
+    // Adds actions
+    ui.actions.addAction('sitrefont', function() {
+        if (graph.isEnabled()) {
+
+            // Sets global styles
+            //var style = graph.getStylesheet().getDefaultEdgeStyle();
+            //style[mxConstants.STYLE_EDGE] = mxEdgeStyle.OrthConnector;
+            // style[mxConstants.STYLE_ROUNDED] = true;
+
+            // style = graph.getStylesheet().getDefaultVertexStyle();
+            // style[mxConstants.STYLE_FILLCOLOR] = '#ffffff';
+            // style[mxConstants.STYLE_SHAPE] = 'swimlane';
+            // style[mxConstants.STYLE_STARTSIZE] = 30;
+            // style[mxConstants.STYLE_DIRECTION] = 'north';
+
+            //style = graph.getStylesheet().getDefaultEdgeStyle();
+            //style[mxConstants.STYLE_TARGET_PORT_CONSTRAINT] = 'north';
+            //style[mxConstants.STYLE_STROKECOLOR] = '#ffffff';
+            //style[mxConstants.STYLE_SOURCE_PORT] = 'south';
+            //graph.setDefaultEdgeStyle(stil);
+            //console.log(stil);
+
+            //style = [];
+            // style[mxConstants.STYLE_SHAPE] = mxConstants.SHAPE_RECTANGLE;
+            // style[mxConstants.STYLE_STROKECOLOR] = 'none';
+            // style[mxConstants.STYLE_FILLCOLOR] = 'none';
+            // style[mxConstants.STYLE_FOLDABLE] = false;
+            // graph.getStylesheet().putCellStyle('column', style);
+
+            // graph.alternateEdgeStyle = 'targetPortConstraint=west';
+
+            var edgeStyle = graph.getStylesheet().getDefaultEdgeStyle();
+            edgeStyle[mxConstants.STYLE_EDITABLE] = 0;
+            edgeStyle[mxConstants.STYLE_RESIZABLE] = 0;
+            //edgeStyle[mxConstants.STYLE_STROKECOLOR] = "#d4d4d4";
+            edgeStyle[mxConstants.STYLE_ORTHOGONAL] = 0;
+            edgeStyle[mxConstants.STYLE_STROKEWIDTH] = 1;
+            edgeStyle[mxConstants.STYLE_BENDABLE] = 1;
+            edgeStyle[mxConstants.STYLE_ROUNDED] = true;
+            edgeStyle[mxConstants.STYLE_EDGE] = mxConstants.EDGESTYLE_ENTITY_RELATION;
+            edgeStyle[mxConstants.LABEL_HANDLE_SIZE] = 50;
+            edgeStyle[mxConstants.HANDLE_FILLCOLOR] = '#000000'
+            graph.getStylesheet().putCellStyle("edge_style", edgeStyle);
+
+            graph.refresh(); // update the graph
+        }
+
+    }, null, null, 'v3.0');
+
+
 
 });
