1eda7a878e102f7c8f21899af4a5a1ccae7420b5
CoolZeroNL <info@local-events.nl>
2019-10-10 09:21:27 +0200

    Auto Add: 10-10-2019 09:21:27 [skip ci]

--- # Draw-io-plugin-SecurIT/draw-io-plugin-securit-huisstyle-v1.js
+++ # Draw-io-plugin-SecurIT/draw-io-plugin-securit-huisstyle-v1.js
@@ -7,12 +7,7 @@ function appendStyle(styles) {
     else css.appendChild(document.createTextNode(styles));
 
     document.getElementsByTagName("head")[0].appendChild(css);
-
-    // replace 2019 - > 2020
-    // document.body.innerHTML = document.body.innerHTML.replace('2019', '2020');
 }
-
-// document.body.innerHTML = document.body.innerHTML.replace('hello', 'hi');
 
 
 var styles = `
