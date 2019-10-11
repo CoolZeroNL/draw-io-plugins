ebb92ab74c6ec7243007d2f2a3f030a52567bbdb
CoolZeroNL <info@local-events.nl>
2019-10-11 14:20:25 +0200

    Auto Add: 11-10-2019 14:20:25 [skip ci]

--- # Draw-io-plugin-Cyberark-Icons/draw-io-plugin-cyberark-icons-v1.js
+++ # Draw-io-plugin-Cyberark-Icons/draw-io-plugin-cyberark-icons-v1.js
@@ -4,45 +4,45 @@ Draw.loadPlugin(function(ui) {
     ui.sidebar.addStencilPalette('flowchart', 'Cyberark Icons Components', 'https://yacdn.org/serve/https://raw.githubusercontent.com/CoolZeroNL/draw-io-plugins/master/%23%20Draw-io-plugin-Cyberark-Icons/stencil-cyberark-Components-v1.xml?maxAge=10', ';fillColor=#ffffff;strokeColor=#000000;strokeWidth=2');
 
     // Collapses default sidebar entry and inserts this before
-    var c = ui.sidebar.container;
-    c.firstChild.click();
-    c.insertBefore(c.lastChild, c.firstChild);
-    c.insertBefore(c.lastChild, c.firstChild);
+    // var c = ui.sidebar.container;
+    // c.firstChild.click();
+    // c.insertBefore(c.lastChild, c.firstChild);
+    // c.insertBefore(c.lastChild, c.firstChild);
 
     // Adds custom sidebar entry
     ui.sidebar.addStencilPalette('flowchart', 'Cyberark Icons Flow', 'https://yacdn.org/serve/https://raw.githubusercontent.com/CoolZeroNL/draw-io-plugins/master/%23%20Draw-io-plugin-Cyberark-Icons/stencil-cyberark-Flow-v1.xml?maxAge=10', ';fillColor=#ffffff;strokeColor=#000000;strokeWidth=2');
 
     // Collapses default sidebar entry and inserts this before
-    var c = ui.sidebar.container;
-    c.firstChild.click();
-    c.insertBefore(c.lastChild, c.firstChild);
-    c.insertBefore(c.lastChild, c.firstChild);
+    // var c = ui.sidebar.container;
+    // c.firstChild.click();
+    // c.insertBefore(c.lastChild, c.firstChild);
+    // c.insertBefore(c.lastChild, c.firstChild);
 
     // Adds custom sidebar entry
     ui.sidebar.addStencilPalette('flowchart', 'Cyberark Icons Levels', 'https://yacdn.org/serve/https://raw.githubusercontent.com/CoolZeroNL/draw-io-plugins/master/%23%20Draw-io-plugin-Cyberark-Icons/stencil-cyberark-Levels-v1.xml?maxAge=10', ';fillColor=#ffffff;strokeColor=#000000;strokeWidth=2');
 
     // Collapses default sidebar entry and inserts this before
-    var c = ui.sidebar.container;
-    c.firstChild.click();
-    c.insertBefore(c.lastChild, c.firstChild);
-    c.insertBefore(c.lastChild, c.firstChild);
+    // var c = ui.sidebar.container;
+    // c.firstChild.click();
+    // c.insertBefore(c.lastChild, c.firstChild);
+    // c.insertBefore(c.lastChild, c.firstChild);
 
     // Adds custom sidebar entry
     ui.sidebar.addStencilPalette('flowchart', 'Cyberark Icons Logos', 'https://yacdn.org/serve/https://raw.githubusercontent.com/CoolZeroNL/draw-io-plugins/master/%23%20Draw-io-plugin-Cyberark-Icons/stencil-cyberark-Logos-v1.xml?maxAge=10', ';fillColor=#ffffff;strokeColor=#000000;strokeWidth=2');
 
     // Collapses default sidebar entry and inserts this before
-    var c = ui.sidebar.container;
-    c.firstChild.click();
-    c.insertBefore(c.lastChild, c.firstChild);
-    c.insertBefore(c.lastChild, c.firstChild);
+    // var c = ui.sidebar.container;
+    // c.firstChild.click();
+    // c.insertBefore(c.lastChild, c.firstChild);
+    // c.insertBefore(c.lastChild, c.firstChild);
 
     // Adds custom sidebar entry
     ui.sidebar.addStencilPalette('flowchart', 'Cyberark Icons Others', 'https://yacdn.org/serve/https://raw.githubusercontent.com/CoolZeroNL/draw-io-plugins/master/%23%20Draw-io-plugin-Cyberark-Icons/stencil-cyberark-Others-v1.xml?maxAge=10', ';fillColor=#ffffff;strokeColor=#000000;strokeWidth=2');
 
     // Collapses default sidebar entry and inserts this before
-    var c = ui.sidebar.container;
-    c.firstChild.click();
-    c.insertBefore(c.lastChild, c.firstChild);
-    c.insertBefore(c.lastChild, c.firstChild);
+    // var c = ui.sidebar.container;
+    // c.firstChild.click();
+    // c.insertBefore(c.lastChild, c.firstChild);
+    // c.insertBefore(c.lastChild, c.firstChild);
 
 });
