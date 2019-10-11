847b8b2e95f627d20c0b6d34d4d0eef8c5649323
CoolZeroNL <info@local-events.nl>
2019-10-11 14:25:53 +0200

    Auto Add: 11-10-2019 14:25:53 [skip ci]

--- # Draw-io-plugin-Cyberark-Icons/draw-io-plugin-cyberark-icons-v1.js
+++ # Draw-io-plugin-Cyberark-Icons/draw-io-plugin-cyberark-icons-v1.js
@@ -44,5 +44,6 @@ Draw.loadPlugin(function(ui) {
     c.firstChild.click();
     c.insertBefore(c.lastChild, c.firstChild);
     c.insertBefore(c.lastChild, c.firstChild);
+    c.firstChild.click();
 
 });
