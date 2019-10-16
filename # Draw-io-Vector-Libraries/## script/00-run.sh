551c7fcff7deae747822eb7d31a08918b2afd829
CoolZeroNL <info@local-events.nl>
2019-10-16 07:59:47 +0200

    Auto Add: 16-10-2019 07:59:46 [skip ci]

--- # Draw-io-Vector-Libraries/## script/00-run.sh
+++ # Draw-io-Vector-Libraries/## script/00-run.sh
@@ -1,65 +1,5 @@
 #!/bin/bash
 
-
-# #TODO
-# bug: all svg files can not have a space in the filename
-
-
-## For Each Folder in Cyberark.Images 
-ls -F ../../\#\ Draw-io-plugin-Cyberark-Icons/cyberark.images/ | grep / | sed 's|/||g' | sort -r | while read folder
-do
-
-	name='library-cyberark-'$folder'-v1'
-	filename=$name'.xml'
-    
-
-    echo $name
-    echo $filename
-
-    echo '<mxlibrary title="Cyberark Icons '$folder'">[' > ../$filename
-
-        counterimg=`ls ../../\#\ Draw-io-plugin-Cyberark-Icons/cyberark.images/$folder | grep .svg | wc -l`
-        counter='0'
-
-        ls ../../\#\ Draw-io-plugin-Cyberark-Icons/cyberark.images/$folder | grep .svg | while read line
-        do
-            counter=$((counter+1))
-            svgfilename=${line%.*}
-            size=$(identify ../../\#\ Draw-io-plugin-Cyberark-Icons/cyberark.images/$folder/$line)
-            width=`echo $size | awk '{print $4}' | awk -F'x' '{print $1}'`
-            height=`echo $size | awk '{print $4}' | awk -F'x' '{print $2}'`
-
-            base64svgimg=$(cat ../../\#\ Draw-io-plugin-Cyberark-Icons/cyberark.images/$folder/$line | base64 -w 0)
-            
-            sed 's/{WIDTH}/'$width'/' basemodel > basemodel_tmp
-            sed -i 's/{HEIGHT}/'$height'/' basemodel_tmp
-            sed -i 's@{BASE64SVG}@'"${base64svgimg}"'@g' basemodel_tmp
-
-            ./02-urlencode.sh
-            ./04-base64-deflate.pl > basemodel_done
-
-            # append libray file
-            if [[ "$counter" == "$counterimg" ]]; then
-            append=''
-            else
-            append=','
-            fi
-
-            echo '{"xml":"'`cat basemodel_done`'","w":'$width',"h":'$height',"aspect":"fixed","title":"'$svgfilename'"}'$append'' >> ../$filename
-
-            # exit 1
-        done
-
-    echo ']</mxlibrary>' >> ../$filename
-    # exit 1
-done
-
-
-   
-
-
-
-
 # 0. generate library-xx.xml
 # 1. for each SVG
 
@@ -68,6 +8,65 @@ done
     # 1.2 append basemodel -> basemodel_tmp
     # 1.3 02-urlencode.sh basemodel_tmp -> basemodel_tmp_urlencoded
     # 1.4 04-base64-deflate.pl basemodel_tmp_urlencoded --> append library file...
+
+path='../../# Draw-io-plugin-Cyberark-Icons/cyberark.images-v2/'
+
+## For Each Folder in Cyberark.Images 
+ls -F "$path" | grep / | sed 's|/||g' | sort -r | while read folder
+do
+
+    # Declare filenames
+	name='library-cyberark-'$folder'-v1'
+	filename=$name'.xml'
+   
+    echo "Building:" $filename
+
+    echo '<mxlibrary title="Cyberark Icons '$folder'">[' > ../$filename
+
+        # Count the images.
+        counterimg=`ls "$path"/"$folder" | grep .svg | wc -l`
+        counter='0'
+
+        ls "$path"/"$folder" | grep .svg | while read line
+        do
+            counter=$((counter+1))
+            svgfilename=${line%.*}
+            title=$(echo $svgfilename | sed 's/_/ /g')
+            size=$(identify "$path"/"$folder"/"$line")
+            width=`echo $size | awk '{print $4}' | awk -F'x' '{print $1}'`
+            height=`echo $size | awk '{print $4}' | awk -F'x' '{print $2}'`
+
+            base64svgimg=$(cat "$path"/"$folder"/"$line" | base64 -w 0)
+            
+            sed 's/{WIDTH}/'$width'/' basemodel > basemodel_tmp
+            sed -i 's/{HEIGHT}/'$height'/' basemodel_tmp
+            sed -i 's@{BASE64SVG}@'"${base64svgimg}"'@g' basemodel_tmp
+
+            # URL Encode
+            ./02-urlencode.sh
+
+            # Deflate and Base64 Encode
+            ./04-base64-deflate.pl > basemodel_done
+
+            # append libray file
+            if [[ "$counter" == "$counterimg" ]]; then
+                append=''
+            else
+                append=','
+            fi
+
+            echo '{"xml":"'`cat basemodel_done`'","w":'$width',"h":'$height',"aspect":"fixed","title":"'$title'"}'$append'' >> ../$filename
+
+        done
+
+    echo ']</mxlibrary>' >> ../$filename
+
+done
+
+
+   
+
+
 
 
 
